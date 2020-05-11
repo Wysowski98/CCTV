085c1cf4c89666a96a358d04398171211ccaac13
Anna Nawrot <nawrociia@gmail.com>
2020-05-11 21:31:33 +0200

    Formatka zalogowanego użytkownika

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+          
+            GridCursor.Margin = new Thickness(0, (80 * index) , 0, 0 );
+            UserControl usc = null;
+            Cotu.Children.Clear();
+            switch (index)
+            {
+                case 0:
+                    usc = new Transmission();
+                    Cotu.Children.Add(usc);
+                    break;
+
+
+                case 3:
+                    usc = new password();
+                    Cotu.Children.Add(usc);
+                    break;
+
+            }
+        }
+    }
+}
