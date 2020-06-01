d3da679368ba49df9a0467d327e444cc61d0c4f2
Radosław Płachta <radplaoffice@gmail.com>
2020-06-01 11:15:06 +0200

    Unifikowanie osobnych solucji w jedną dotyczącą formatek

--- CameraWPF/VideoRecorder/InputBox.cs
+++ CameraWPF/VideoRecorder/InputBox.cs
@@ -3,7 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace VideoRecorder
+namespace CCTVSystem.Client
 {
     internal class InputBox
     {
--- CameraWPF/VideoRecorder/MainWindow.xaml.cs
+++ CameraWPF/VideoRecorder/MainWindow.xaml.cs
@@ -13,20 +13,52 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace VideoRecorder
+namespace CCTVSystem.Client
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    /// 
-
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new MainWindowViewModel(panelImages);
-            //this.Closing += (s, e) => (this.DataContext as IDisposable).Dispose();                            
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+          
+            GridCursor.Margin = new Thickness(0, (80 * index) , 0, 0 );
+            UserControl usc = null;
+            Cotu.Children.Clear();
+            switch (index)
+            {
+           
+                case 1:
+                    usc = new Transmission();
+                    Cotu.Children.Add(usc);
+                    break;
+
+                case 2:
+                    usc = new History();
+                    Cotu.Children.Add(usc);
+                    break;
+
+                case 3:
+                    usc = new DeleteRecords();
+                    Cotu.Children.Add(usc);
+                    break;
+
+                case 4:
+                    usc = new password();
+                    Cotu.Children.Add(usc);
+                    break;
+
+               case 5:
+                    usc = new InfoPage();
+                    Cotu.Children.Add(usc);
+                    break;
+            }
         }
     }
 }
