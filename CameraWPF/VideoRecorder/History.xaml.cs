37a60f0e2c1db672791239d04f8d5eae07e46e9f
Anna Nawrot <nawrociia@gmail.com>
2020-06-15 02:30:09 +0200

    user + record

---
+++ CameraWPF/VideoRecorder/History.xaml.cs
@@ -0,0 +1,44 @@
+﻿using CCTVSystem.Client.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace CCTVSystem.Client
+{
+    public partial class History : UserControl
+    {
+        static HttpClient client = new HttpClient();
+       
+        private GetUserProfileCommand _profile;
+        public class watchingHistory
+        {
+            public int Id { get; set; }
+            public string Data { get; set; }
+            public bool Recorded { get; set; }
+        }
+
+        public History()
+        {
+            InitializeComponent();
+            List<watchingHistory> items = new List<watchingHistory>();
+            items.Add(new watchingHistory() { Id = 12, Data = "04.05.2020", Recorded = true });
+            items.Add(new watchingHistory() { Id = 3, Data = "03.05.2020", Recorded = false });
+            items.Add(new watchingHistory() { Id = 5, Data = "06.05.2020", Recorded = false });
+            Watched.ItemsSource = items;
+        }
+       
+    }
+}
