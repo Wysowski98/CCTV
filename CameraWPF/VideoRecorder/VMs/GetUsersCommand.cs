8d271a041d52a32c25934f3c339390c76b43c001
domikul <kulczykdominikaa@gmail.com>
2020-09-08 21:48:22 +0200

    delete user cascade

---
+++ CameraWPF/VideoRecorder/VMs/GetUsersCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCTVSystem.Client.ViewModels
+{
+    class GetUsersss
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        //public IList<string> Roles { get; set; }
+        public int RoleId { get; set; }
+    }
+}
